4344dccf7ee26d1057121ea3ef7cfa82d8024b8c
AaronF11 <aaron.flores7908@alumnos.udg.mx>
2022-11-18 00:58:31 -0600

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora_de_ecuaciones_diferenciales
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
