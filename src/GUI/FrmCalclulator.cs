459f42950dde9dd6105bfede7369280d5263487f
AaronF11 <aaron.flores7908@alumnos.udg.mx>
2022-11-24 23:01:25 -0600

    Implementación del redondeo del FrmCalclulator
        Agregue documentación de los colaboradores e implemente la función para redondeo del FrmCalclulator.

--- src/GUI/FrmCalclulator.cs
+++ src/GUI/FrmCalclulator.cs
@@ -4,17 +4,67 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace Calculadora_de_ecuaciones_diferenciales
 {
-    public partial class FrmPrincipal : Form
+    /// <summary>
+    /// Desarrolladores:
+    /// @Aarón Flores Pasos
+    /// @Aram Josue Núñez Gradilla
+    /// @Emilio Segura
+    /// @Jose Luis Robles Peña
+    /// @Luis Gonzalo Rivera Andrade
+    /// @Luis Manuel Mantecón Reynaldo
+    /// @Christian Ulises Espinoza Huerta
+    /// </summary>
+
+    //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    //Clase FrmCalculator que muestra la interfaz de la calculadora.
+    //Fecha: 24/11/2022
+    //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    public partial class FrmCalculator : Form
     {
-        public FrmPrincipal()
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        //Atributos.
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        //Contructor.
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        public FrmCalculator()
         {
             InitializeComponent();
+            Redondear();
         }
+
+        #region Método para redondear componentes.
+        //---------------------------------------------------------------------
+        //Crea un rectangulo para redondear el componente.
+        //---------------------------------------------------------------------
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+            (
+            int nLeftRect,
+            int nTopRect,
+            int nRightRect,
+            int nBottomRect,
+            int nWidthEllipse,
+            int nHeightEllipse
+            );
+
+        //---------------------------------------------------------------------
+        //Redondea los bordes de los componentes.
+        //---------------------------------------------------------------------
+        private void Redondear()
+        {
+            //Componentes a los que se le aplica el redondeo.
+            Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 25, 25));
+        }
+        #endregion
     }
 }
