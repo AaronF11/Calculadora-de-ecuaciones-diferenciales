e2f197498424ccd67edcf6181ec5ee955b87510d
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2022-11-30 08:57:01 -0600

    Add - implementar caracteristicas clase padre

--- src/Base/Metodos/CExacta.cs
+++ src/Base/Metodos/CExacta.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Calculadora_de_ecuaciones_diferenciales.src.Base.Metodos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +10,7 @@ namespace Calculadora_de_ecuaciones_diferenciales.src.Base.Mrtodos
     //----------------------------------------------------------//
     // Clase que répresenta el método para ecuaciones exactas
     //----------------------------------------------------------//
-    public class CExacta
+    public class CExacta : CMetodo
     {
         //-------------------------
         // Atributos
@@ -18,9 +19,14 @@ namespace Calculadora_de_ecuaciones_diferenciales.src.Base.Mrtodos
         //-------------------------
         // Constructor
         //-------------------------
-        public CExacta()
+        public CExacta(string Ecuacion) : base(Ecuacion)
         {
 
+        }
+
+        public override void PartirEcuacion()
+        {
+            throw new NotImplementedException();
         }
     }
 }
