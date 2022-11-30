ea68a4362dca4388480cdf91667b71b34a756f18
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2022-11-30 08:38:26 -0600

    Add - clase método
    	Esta clase es padre de las clases utilizadas para
    	resolver las ecuaciones diferenciales, esta contendra
    	la forma base para obtener las partes de la ecuacion,
    	los signos, y como resolverse.

---
+++ src/Base/Metodos/CMétodo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_de_ecuaciones_diferenciales.src.Base.Metodos
+{
+    //+-------------------------------------------------------+//
+    //| Clase que representa los atributos base               |
+    //| para ser utilizados en los métodos de resolución para |
+    //| ecuaciones dif.                                       |
+    //+-------------------------------------------------------+//
+    public class CMetodo
+    {
+        //-------------------------
+        // Atributos
+        //-------------------------
+
+        //-------------------------
+        // Constructor
+        //-------------------------
+        public CMetodo()
+        {
+
+        }
+    }
+}
