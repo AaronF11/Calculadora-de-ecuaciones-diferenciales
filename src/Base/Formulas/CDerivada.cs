d16d13ae16ecf022c0be6c77a49f8f9629658563
Juan Antonio Gil Lopez <juan.gil7919@alumnos.udg.mx>
2022-12-03 13:32:18 -0600

    Formulas de derivacion (de la 1 a la 5 que nos asignaron) terminadas por Jorge Luis y Juan Antonio.

--- src/Base/Formulas/CDerivada.cs
+++ src/Base/Formulas/CDerivada.cs
@@ -9,7 +9,7 @@ namespace Calculadora_de_ecuaciones_diferenciales.src.Base.Formulas
     //---------------------------------------------------------------------
     //Clase que Deriva.
     // CMJL. Jorge Luis Cruz Macias. 28/11/2022
-    // JAGL. Juan Antonio Gil Lopez.
+    // JAGL. Juan Antonio Gil Lopez. 28/11/2022
     //---------------------------------------------------------------------
 
     public class CDerivada
@@ -24,6 +24,7 @@ namespace Calculadora_de_ecuaciones_diferenciales.src.Base.Formulas
 
         //-----------------------------------------------------//
         //Método que resuelve derivada: Potencia f(x)= n * x^n-1
+        //Jorge Luis Cruz
         //-----------------------------------------------------//
         public static string Derivar(string Monomio)
         {
@@ -107,6 +108,36 @@ namespace Calculadora_de_ecuaciones_diferenciales.src.Base.Formulas
             }
             return ResultadoDeFuncion;
         }
+        //-------------------------------------------------------------------------------
+        //Formula 3 que resuelve F = k  ---> F'=0
+        //Jorge Luis Cruz
+        //-------------------------------------------------------------------------------
+        public static string Formula3(string mono3)
+        {
+            //Variables Datos nesesarios para realizar derivada
+            int Coeficiente;
+            int Exponente;
+            String Variable;
+
+            String Respuesta;
+
+            //Inicialización 
+
+            Coeficiente = 0;
+            Exponente = 0;
+            Variable = "";
+            Respuesta = "";
+
+            if (Exponente == 0)
+            {
+                if (Variable == null)
+                {
+                    Coeficiente = 0;
+                    Respuesta = Coeficiente.ToString();
+                }
+            }
+            return Respuesta;
+        }
         //---------------------------------------------------------------------------//
         //Método que resuelve la formula 4: "y = x es igual a y = 1" 
         //Juan Antonio Gil Lopez
@@ -146,66 +177,47 @@ namespace Calculadora_de_ecuaciones_diferenciales.src.Base.Formulas
         public static string Formula5(string Monomio5)
         {
             //----------------------------------------------------------
-            //Esta formula toma como base parte de la primera formula
+            //Esta formula toma como base a la primera formula
             //----------------------------------------------------------
 
             //Variables 
             int Coeficiente;
-            int Exponente;
+            int ExponenteMonomio;
             String Variable;
             String SignoExp;
             int DerivadaDeVariable;
             int ExponenteDeVariable;
-
-            string ResultadoDeFuncion;
-
-            //Inicialización 
-            Coeficiente = 0;
-            Exponente = 0;
-            SignoExp = "";
-            Variable = "";
+            int ResultadoDeFuncion;
 
             int NuevoCoeficiente;  //--> Resultado de la multiplicación Exponente por coeficiente
             int REExponente;       //--> Resultado de la resta al Exponente 
 
             String ResultadoDerivada;//--> Contiene la cadena de caracteres la cual representa el resultado de una derivada
 
-            //Contiene el Valor del exponente en tipo String
-            String ExponenteSt;
+            //Inicialización 
+            Coeficiente = 0;
+            ExponenteMonomio = 0; //--> Este exponente es el que tiene el monomio (en la formula se pone como "n").
+            SignoExp = "";
+            Variable = "";
+            ExponenteDeVariable = 0; //--> Este exponente es el que tiene la variable "x".
 
-            //Se realizan las operaciones básicas 
-            NuevoCoeficiente = Exponente * Coeficiente;
-            REExponente = Exponente - 1;
-            DerivadaDeVariable = Coeficiente * Exponente;
-     
-            //EN CONSTRUCCION
+            //Se realiza el metodo de la primera formula
+            NuevoCoeficiente = ExponenteMonomio * Coeficiente; //n * v
+            REExponente = ExponenteMonomio - 1; //v^n-1
 
-            
+            //Aqui se deriva v
+            DerivadaDeVariable = Coeficiente * ExponenteDeVariable; //v * m
+            ExponenteDeVariable = ExponenteDeVariable - 1;    //m - 1
 
+            //Se multiplica el nuevo coeficiente por la derivada v
+            ResultadoDeFuncion = NuevoCoeficiente * DerivadaDeVariable; //v2 * v1
 
             //Se le asigno el valor a SignoExp
             SignoExp = "^";
 
-            //Si el Exponente =1 , el Exponente no se pone 
-
-            if (REExponente == 1)
-            {
-                ExponenteSt = Exponente.ToString();
-                ExponenteSt = "";
-                SignoExp = "";
-            }
-            //Si es diferente a 1, Si se pone exponente y se coloca el signo "^"
-            else
-            {
-                ExponenteSt = REExponente.ToString();
-                SignoExp = "^";
-            }
-
             //Muestra el Resultado
-            return ResultadoDerivada = Coeficiente.ToString() + Variable + SignoExp + ExponenteSt;
-
+            return ResultadoDerivada = "(" + ResultadoDeFuncion + Variable + SignoExp + ExponenteDeVariable + ")" + SignoExp + REExponente;
         }
-
     }
 }
 
